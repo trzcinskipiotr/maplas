7b7ec58be93a03aef3a22ab174deb0a36aa6979e
Piotr Trzcinski <>
2019-04-22 11:28:09 +0200

    Panel boczny.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,21 @@
 
 // eslint-disable-next-line
 import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
 import 'leaflet.gridlayer.googlemutant'
 import loadgoogle from 'load-google-maps-api'
 import Vue from 'vue'
 import App from '@/App'
 import router from '@/js/router'
+import BootstrapVue from 'bootstrap-vue'
+import 'leaflet-easybutton'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'leaflet/dist/leaflet.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 loadgoogle()
 
 /* eslint-disable no-new */
