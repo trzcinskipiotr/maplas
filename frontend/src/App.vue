7494e653812759a79316bfc36970a3989f16319f
Piotr Trzcinski <>
2019-07-30 10:10:29 +0200

    lint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -184,8 +184,8 @@ export default {
       })
 
       layers['hyddaBase'] = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
-	      maxZoom: 18,
-	      attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+        attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         errorTileUrl: 'static/tiledownloadfailed.jpg'
       })
 
