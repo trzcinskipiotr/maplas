938a4a39bf6b9708a354102692c61f04a9d51911
Piotr Trzcinski <>
2019-08-28 09:12:15 +0200

    flagi poprawka

--- frontend_ts/src/ts/Track.ts
+++ frontend_ts/src/ts/Track.ts
@@ -15,16 +15,16 @@ export default class Track {
       smoothFactor: 1,
     });
     const startIcon = L.icon({
-      iconUrl: 'img/startflag2.png',
-      iconAnchor: [5, 32],
+      iconUrl: 'img/startflag3.png',
+      iconAnchor: [30, 45],
     });
     const finishIcon = L.icon({
-      iconUrl: 'img/finishflag2.png',
-      iconAnchor: [5, 32],
+      iconUrl: 'img/finishflag3.png',
+      iconAnchor: [17, 45],
     });
     const tmpArray = this.gpsTrack.pointsArray;
-    this.startMarker = new L.Marker([tmpArray[0].lat, tmpArray[0].lng], {icon: startIcon});
-    this.finishMarker = new L.Marker([tmpArray[tmpArray.length - 1].lat, tmpArray[tmpArray.length - 1].lng], {icon: finishIcon});
+    this.startMarker = new L.Marker([tmpArray[0].lat, tmpArray[0].lng], {icon: startIcon, zIndexOffset: 1});
+    this.finishMarker = new L.Marker([tmpArray[tmpArray.length - 1].lat, tmpArray[tmpArray.length - 1].lng], {icon: finishIcon, zIndexOffset: 2});
   }
 
 }
