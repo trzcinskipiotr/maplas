bbdc415f907e319b66c42a35d0314513d0816fb6
Piotr Trzcinski <>
2019-09-25 13:30:10 +0200

    Szczegoly w adminie.

--- backend/src/maplas_app/admin.py
+++ backend/src/maplas_app/admin.py
@@ -5,7 +5,7 @@ from maplas_app.models import Track, Place
 
 @admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'color', 'start_time', 'end_time', 'distance', 'status', 'type', 'get_points_json_counts', 'get_points_json_optimized_counts', 'get_gpx_file_counts')
+    list_display = ('id', 'name', 'description', 'color', 'start_time', 'end_time', 'distance', 'status', 'type')
     search_fields = ('id', 'name', 'description', 'color', 'points_json')
 
 @admin.register(Place)
@@ -13,3 +13,11 @@ class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('id', 'name')
 
+class TrackDetails(Track):
+    class Meta:
+        proxy = True
+
+@admin.register(TrackDetails)
+class TrackDetailsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'color', 'start_time', 'end_time', 'distance', 'status', 'type', 'get_points_json_counts', 'get_points_json_optimized_counts', 'get_gpx_file_counts')
+    search_fields = ('id', 'name', 'description', 'color', 'points_json')
