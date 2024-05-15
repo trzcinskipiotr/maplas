79d75bbc1a54d04d664815905f9fc3888282f20a
Piotr Trzcinski <>
2024-05-15 12:00:26 +0200

    Leaflet offline

--- frontend_ts/src/ts/router.ts
+++ frontend_ts/src/ts/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Index from '@/views/Index.vue';
+import IndexOff from '@/views/IndexOff.vue';
 import Test from '@/views/Test.vue';
 
 Vue.use(Router);
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index,
+    },
+    {
+      path: '/off',
+      name: 'indexoff',
+      component: IndexOff,
     },
     {
       path: '/test',
